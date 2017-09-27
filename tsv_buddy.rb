d4785639f238adeb80bf57eefd52c66be5ab115b
CTLIU <chiatungliu@gmail.com>
2017-09-28 00:27:36 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,39 @@
+LINE = "\n".freeze
+TAB = "\t".freeze
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    @data = []
+    tsv_in_lines = tsv.split(LINE)
+    keys = tsv_in_lines[0].split(TAB)
+    # keys.map!(&:chomp)
+    tsv_in_lines.shift
 
+    tsv_in_lines.each do |line|
+      values = line.split(TAB)
+      people = {}
+      keys.each_index { |index| people[keys[index]] = values[index].chomp }
+      @data.push(people)
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    file_out = ''
+    key = @data[0].keys
 
+    file_out = key.reduce { |key1, key2| key1.to_s + TAB + key2.to_s }
+    file_out << LINE
+
+    @data.each do |hash|
+      hash.each_value { |value| file_out << value + TAB }
+      file_out.chop!
+      file_out << LINE
+    end
+    file_out
   end
+  # end of module
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,14 @@
+require 'yaml'
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  def take_yaml(yaml)
+    yaml.to_yaml
+    @data = {}
+    @data = YAML.safe_load(yaml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+  # end of module
+end
